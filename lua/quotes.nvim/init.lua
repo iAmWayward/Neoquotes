bbe371d78f00613e37fdc745a6ed37d806ef5a8f
Wayward <adigitaldruid@proton.me>
2025-07-04 17:20:16 -0400

    Restructure filepath

--- lua/quotes.nvim/init.lua
+++ lua/quotes.nvim/init.lua
@@ -7,7 +7,6 @@ vim.g.loaded_phrase_of_the_day = 1
 return {
   setup = function(opts)
     require("config.config").setup(opts)
-    require("plugins.homemade").setup()
     require("functions.commands").setup()
   end,
 }

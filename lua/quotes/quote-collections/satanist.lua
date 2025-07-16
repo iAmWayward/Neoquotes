afbf95dbd0d034b811ff7f6f104e1acfd6d151b9
wayward <1996lad@pm.me>
2025-07-15 22:50:39 -0400

    Add lots of new quotes

--- lua/quotes/quote-collections/satanist.lua
+++
@@ -1,6 +0,0 @@
-return {
-  {
-    text = "",
-    author = "",
-  },
-}
--- lua/quotes/quote-collections/western-philosophy.lua
+++ lua/quotes/quote-collections/western-philosophy.lua
@@ -4,6 +4,22 @@ return {
     author = "Immanuel Kant",
   },
   {
+    text = "You have power over your mind - not outside events. Realize this, and you will find strength.",
+    author = "Marcus Aurelius",
+  },
+  {
+    text = "The happiness of your life depends upon the quality of your thoughts.",
+    author = "Marcus Aurelius",
+  },
+  {
+    text = "It's not what happens to you, but how you react to it that matters.",
+    author = "Epictetus",
+  },
+  {
+    text = "No one can hurt you without your permission.",
+    author = "Eleanor Roosevelt",
+  },
+  {
     text =
     "I know of no rule which holds so true as that we are always paid for our suspicion by finding what we suspect.",
     author = "Henry David Thoreau",

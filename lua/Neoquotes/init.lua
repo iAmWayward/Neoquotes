806d2d15fac91489cd7408a81c4ad9a873733ec8
wayward <1996lad@pm.me>
2025-07-18 23:50:03 -0400

    test

--- lua/Neoquotes/init.lua
+++ lua/Neoquotes/init.lua
@@ -1,9 +1,9 @@
 local M = {}
 
 function M.setup(opts)
-  if vim.g.loaded_phrase_of_the_day then
-    return
-  end
+  -- if vim.g.loaded_phrase_of_the_day then
+  --   return
+  -- end
   vim.g.loaded_phrase_of_the_day = 1
   require("config").setup(opts)
   require("Neoquotes.commands").setup()

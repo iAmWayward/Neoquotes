972966cb2b0ef34ba8d437830a0f21f5b6df039d
wayward <1996lad@pm.me>
2025-07-19 12:00:02 -0400

    Whoopsies

---
+++ lua/init.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.setup(opts)
+  if vim.g.loaded_phrase_of_the_day then
+    return
+  end
+  vim.g.loaded_phrase_of_the_day = 1
+  require("config").setup(opts)
+  require("Neoquotes.commands").setup()
+end
+
+return M
